86b72e2057dad201e9620194770e44f387d77831
wshchocolatine <wshchocolatine@gmail.com>
2022-02-20 17:27:04 +0100

    change config

--- config/database.ts
+++ config/database.ts
@@ -40,10 +40,7 @@ const databaseConfig: DatabaseConfig = {
         port: Env.get('PG_PORT'),
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
-        database: Env.get('PG_DB_NAME'),
-        ssl: {
-          rejectUnauthorized: false
-        }
+        database: Env.get('PG_DB_NAME')
       },
       migrations: {
         naturalSort: true,
