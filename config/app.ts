ef25159027c531f9b949bb5c331edd5fd5a2455a
wshchocolatine@gmail.com <wshchocolatine@gmail.com>
2022-05-03 23:01:33 +0200

    Fix minor bug, httpOnly cookie false to secure

--- config/app.ts
+++ config/app.ts
@@ -105,8 +105,8 @@ export const http: ServerConfig = {
     domain: '',
     path: '/',
     maxAge: '2h',
-    httpOnly: false,   // <-- here
-    secure: false,     // <-- here
+    httpOnly: true,   // <-- here
+    secure: true,     // <-- here
     sameSite: 'none',
   },
 
